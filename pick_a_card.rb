ebdc4bbce6621d7d3e2ce96f1ebb987bf17f3a8a
italyfm <italyfm@yandex.ua>
2017-07-12 19:27:26 +0300

    Unicode suit symbols

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,12 +1,21 @@
 # Программа, которая выбирает произвольную карту игральной колоды, версия 1.
 #
+# Этот код необходим только при использовании unicode-символов на Windows
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
 
 # Создадаим массив с набором достоинств
 values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
 # Создадим массив с набором мастей.
 # Буби, черви, крести, пики.
-suits = ["Diamonds", "Hearts", "Clubs", "Spades"]
+suits = ["♦", "♠", "♥ ", "♣"]
 
 # Выведем произвольную карты, выбрав по одному элементу из массивов
 puts "#{values.sample} of #{suits.sample}"
